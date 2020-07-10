6adf9efe686b64fde6686676d3a018373312d65e
Hewr Srood <hewr_s@outlook.com>
2020-07-10 19:14:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class Deciduous extends Tree {
 class Evergreen extends Tree {
   constructor( species, name ) {
     super( species )
-    this.name = name
+    this.name = name;
   }
 
   static definition() {
